9045dbdd5979ee33781fafa8b613f7f113769801
Moiz Qureshi <moizq@calpak-usa.com>
2016-04-18 21:52:00 -0700

    Created UI and coded summing of two numbers

--- app/src/main/java/com/example/moizqureshi/sumcalc/MainActivity.java
+++ app/src/main/java/com/example/moizqureshi/sumcalc/MainActivity.java
@@ -8,8 +8,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText numberOne;
+    EditText numberTwo;
+    TextView result;
+    Button btnAdd;
+
+    double num1, num2, sum;
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +41,22 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+
+        numberOne = (EditText) findViewById(R.id.editText);
+        numberTwo = (EditText) findViewById(R.id.editText2);
+        result = (TextView) findViewById(R.id.textView);
+        btnAdd = (Button) findViewById(R.id.add);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1 = Double.parseDouble((numberOne.getText().toString()));
+                num2 = Double.parseDouble(numberTwo.getText().toString());
+                sum = num1 + num2;
+                result.setText("Sum = " + Double.toString(sum));
             }
         });
     }
@@ -49,4 +82,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
